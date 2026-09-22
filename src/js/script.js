073011025dd3c8029956a848ed1bb118e425ad44
Nicolas <nicolas@gmail.com>
2026-09-22 19:05:16 -0300

    Implemento o cadastro do vinho com prompt, alert e console.log.

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+var nomedovinho = prompt("Digite o nome do vinho: ");
+var tipodovinho = prompt("Digite o tipo do vinho: ");
+var safradovinho = prompt("Digite a safra do vinho: ");
+var qtdestoque = prompt("Digite a quantidade em estoque: ");
+
+alert("Cadastro realizado! Veja os detalhes no console.");
+alert("Veja os detalhes do vinho no console: ");
+console.log("Ficha do vinho cadastrado: ")
+console.log("Nome do vinho " + nomedovinho);
+console.log("Tipo: " + tipodovinho);
+console.log("Safra: " + safradovinho)
+console.log("Quantidade em estoque: " + qtdestoque);
